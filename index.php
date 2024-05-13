fefdcc791fd7a14e0c0eb5c948102c9458bc69aa
hashamuddin1 <hashamlaptop@gmail.com>
2024-05-14 02:13:36 +0500

    GET and POST with die

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- get and post super global variable khelata hay -->
+    <!-- get method may url may data ajata hay -->
+    <!-- post method may url may data nhi ata -->
+    <form action="testForm.php" method="post">
+        <input type="text" name="fname" placeholder="First Name"/>
+        <input type="text" name="age" placeholder="Age"/>
+        <input type="submit" name="save">
+    </form>
+</body>
+
+</html>
---
+++ testForm.php
@@ -0,0 +1,6 @@
+<?php
+print_r($_POST);// To show full in array
+echo $_POST['fname'];
+die("Here is some error!!");
+echo $_POST['age'];
+?>
